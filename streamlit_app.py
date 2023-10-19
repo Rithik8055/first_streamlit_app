497af033d75162e9253c4db10e168fb35aff6de3
Rithik8055 <125788151+Rithik8055@users.noreply.github.com>
2023-10-18 23:17:34 -0400

    Update streamlit_app.py
    add header and 3 iteams

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,9 @@
 import streamlit
 streamlit.title('I have a m5')
+
+streamlit.header('🥗Breakfast Menu')
+streamlit.text('🥣Omega 3 & Blueberry Oatmeal')
+streamlit.text('🐔Kale, Spinach & Rocket Smoothie')
+streamlit.text('🥑🍞Hard-Boiled Free-Range Egg')
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
