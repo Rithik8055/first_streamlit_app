d361192fee4c42d6685fff51b4aa78b9ab5598f0
Rithik8055 <125788151+Rithik8055@users.noreply.github.com>
2023-10-23 00:45:09 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 import streamlit
-import pandas
+#import pandas
 import snowflake.connector
+from urllib.error import URLError
 
 streamlit.header('🥗Breakfast Menu')
 streamlit.text('🥣Omega 3 & Blueberry Oatmeal')
@@ -18,7 +19,7 @@ streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado',
 streamlit.dataframe(my_fruit_list)
 
 streamlit.header("Fruityvice Fruit Advice!")
-import requests
+#import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 streamlit.text(fruityvice_response)
 
